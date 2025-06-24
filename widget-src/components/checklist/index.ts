8ce262b20e83b7a4ba1d430d4bdc75ab5248f84b
Mark Learst <learst@me.com>
2025-06-23 23:01:50 -0400

    chore: add barrel files for primitives and checklist components, and top-level components index

---
+++ widget-src/components/checklist/index.ts
@@ -0,0 +1,3 @@
+export { default as ChecklistPanel } from './ChecklistPanel'
+export { default as ChecklistSection } from './ChecklistSection'
+export { default as ChecklistItem } from './ChecklistItem'
---
+++ widget-src/components/primitives/index.ts
@@ -0,0 +1,4 @@
+export { default as AvatarStack } from './AvatarStack'
+export { default as Checkbox } from './Checkbox'
+export { default as ProgressBar } from './ProgressBar'
+export { default as ProgressTracker } from './ProgressTracker'
